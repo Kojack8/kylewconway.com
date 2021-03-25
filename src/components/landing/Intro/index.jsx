4f0e0f2065040b3f649729305a16d60c2c60a520
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-25 17:41:01 -0500

    testing live change

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -15,7 +15,7 @@ export const Intro = () => {
       <IntroWrapper as={Container}>
         <Details theme={theme}>
           <h1>Hi There!</h1>
-          <h4>I’m John and I’m a JAMStack engineer!</h4>
+          <h4>I’m Kyle Conway!</h4>
           <Button as={AnchorLink} href="#contact">
             Hire me
           </Button>
