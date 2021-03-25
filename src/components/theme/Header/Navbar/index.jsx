44b921e978138781966f38e5d3e5d2bbf3a8e92b
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-25 18:29:18 -0500

    name changes

--- src/components/theme/Header/Navbar/index.jsx
+++ src/components/theme/Header/Navbar/index.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <Wrapper as={Container}>
       <Brand as={Link} to="/" theme={theme}>
-        John Doe
+        Kyle Conway
       </Brand>
       <NavbarLinks desktop />
     </Wrapper>
